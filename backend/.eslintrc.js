88a8262e6c785792800d8de69569c8a8b8e9deea
daksh10110 <dakshkanotra@gmail.com>
2023-04-22 01:17:44 +0530

    File Setup is done

---
+++ backend/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	"env": {
+		"node": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"eqeqeq": "error",
+		"no-trailing-spaces": "error",
+		"object-curly-spacing": [
+			"error", "always"
+		],
+		"arrow-spacing": [
+			"error", { "before": true, "after": true }
+		],
+		"no-console": 0
+	}
+}
